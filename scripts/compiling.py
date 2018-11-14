54476969ceb3d067c87c0bede7ef16b2d38e37b4
ibaiape <ibai.apellaniz@gmail.com>
2018-11-14 12:41:17 +0100

    Added CMakeLists.txt and package.xml generation

--- scripts/compiling.py
+++ scripts/compiling.py
@@ -25,6 +25,19 @@ def main(module):
 		print "Chosen platform doesn't exist"
 		sys.exit(1)
 
+
+	os.chdir("templates")
+	with open('package.txt', 'r') as myfile:
+		pkg=myfile.read()
+	pkg = pkg.replace("%PKGNAME%", "hrim_"+module.type+"_"+module.name+"_msgs")
+	pkg = pkg.replace("%PKGDESC%", module.desc)
+
+	with open('cmake.txt', 'r') as myfile:
+		makeFile=myfile.read()
+	makeFile = makeFile.replace("%PKGNAME%", "hrim_"+module.type+"_"+module.name+"_msgs")
+
+	os.chdir(cwd)
+
 	if not os.path.exists(cwd+"/generated"):
 			os.mkdir("generated")
 	os.chdir("generated")
@@ -36,12 +49,20 @@ def main(module):
 	os.chdir(module.name)
 	cwd = os.getcwd()
 
+	msgFiles = ""
+
 	for topic in module.topics:
 		os.chdir(cwd)
 		if len(topic.origin)==0:
 			folderPath = os.getcwd()+"/hrim_"+module.type+"_"+module.name+"_msgs/msg"
 			if not os.path.exists(folderPath):
 				os.makedirs(folderPath)
+
+			os.chdir(folderPath[:-4])
+			package = open("package.xml", "w")
+			package.write(pkg)
+			package.close()
+
 			os.chdir(folderPath)
 			msg = ""
 			if topic.desc is not None and len(topic.desc)>0:
@@ -51,7 +72,15 @@ def main(module):
 					for value in sorted( ((v,k) for k,v in prop.enumeration.iteritems())):
 						msg+=prop.type+" "+value[1]+"="+str(value[0])+"\n"
 				msg+=dataTypes[prop.type]+("[{}] ".format(prop.length) if prop.array else " ")+prop.name+((" # "+prop.desc) if prop.desc is not None else "")+"\n\n"
-			filepath = topic.name+".msg"
+			filepath = topic.name.title()+".msg"
 			text_file = open(filepath, "w")
 			text_file.write(msg)
 			text_file.close()
+			msgFiles+="\t\"msg/"+filepath+"\"\n"
+
+	makeFile = makeFile.replace("%PKGFILES%", msgFiles[:-1])
+
+	os.chdir(folderPath[:-4])
+	cmake = open("CMakeLists.txt", "w")
+	cmake.write(makeFile)
+	cmake.close()
