fd45dbb206f60aaee4b494cd394e03537146397e
ibaiape <ibai.apellaniz@gmail.com>
2018-11-16 18:19:46 +0100

    Added shorthads to generate all existing modules

--- hrim.py
+++ hrim.py
@@ -3,6 +3,7 @@ sys.path.insert(0, 'scripts')
 import printing
 import parsing
 import compiling
+import os
 from classes import *
 
 def main(args):
@@ -20,16 +21,36 @@ def main(args):
 		printing.main(module, extend)
 	elif args[1] == "generate":
 		extend = False
-		# if topic/properties not defined on the parsed file should show their full content or not when printing
-		if len(args) == 4:
-			if args[3] == "1":
-				extend = True
 		# check for file input
 		if len(args) < 3:
 			print "Please input file to parse"
 			sys.exit()
-		module = parsing.main(args[2])
-		compiling.main(module)
+		if args[2] == "all":
+			path = os.getcwd()
+			for file in [
+				path+"/models/actuator/gripper/gripper.xml",
+				path+"/models/actuator/servo/servo.xml",
+				path+"/models/sensor/gasdetector/gasdetector.xml",
+				path+"/models/sensor/rangefinder/rangefinder.xml",
+				path+"/models/sensor/torque/torque.xml"
+			]:
+				os.chdir(path)
+				module = parsing.main(file)
+				compiling.main(module)
+		elif args[2] == "allClean":
+			path = os.getcwd()
+			for file in [
+				path+"/models/actuator/gripper/gripper_clean.xml",
+				path+"/models/actuator/servo/servo_clean.xml",
+				path+"/models/sensor/rangefinder/rangefinder_clean.xml",
+				path+"/models/sensor/torque/torque_clean.xml"
+			]:
+				os.chdir(path)
+				module = parsing.main(file)
+				compiling.main(module)
+		else:
+			module = parsing.main(args[2])
+			compiling.main(module)
 	else:
 		print "Unknown command"
 
