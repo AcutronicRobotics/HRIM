fcde5188058a37b9d48d0e1b7d4b266bf7f7470c
Asier Bilbao <abilbaotm@gmail.com>
2019-02-14 12:20:19 +0100

    check that hrim CLI installs in travis

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/root/ros2_ws/src/hrim
-python3 hrim.py generate --platform ros2 all
+sudo pip3 install -r requirements.txt
+
+hrim generate --platform ros2 all
 
 /bin/bash -c "source /opt/ros/crystal/setup.bash && colcon build --merge-install"
