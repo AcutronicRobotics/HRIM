3078e1850cd55f293e15bdff6d01beff998e266e
Asier Bilbao <abilbaotm@gmail.com>
2019-02-14 09:45:53 +0100

    generate HRIM packages

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ls /home/root/ros2_ws/src/hrim erlerobot/hrim
+cd /home/root/ros2_ws/src/hrim
+python3 hrim.py generate --platform ros2 all
 
 /bin/bash -c "source /opt/ros/crystal/setup.bash && colcon build --merge-install"
