613e21e42fd683675cf9929da962230b5f063aaf
Asier Bilbao <abilbaotm@gmail.com>
2019-02-14 12:44:59 +0100

    hotfix travis, colcon not detecting pkgs

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -4,5 +4,7 @@ pip3 install -r requirements.txt
 python3 setup.py install
 
 hrim generate --platform ros2 all
+cd generated
+ls
 
 /bin/bash -c "source /opt/ros/crystal/setup.bash && colcon build --merge-install"
