33efced25678886126955d06ff5a58d52040a453
LanderU <lander.usategui@gmail.com>
2019-07-19 12:28:52 +0200

    Added /bin
    Signed-off-by: LanderU <lander.usategui@gmail.com>

--- datasheet_generator/datasheet_generator.py
+++ datasheet_generator/datasheet_generator.py
@@ -96,8 +96,8 @@ if __name__ == "__main__":
     f.write(content)
     f.close()
 
-    cmd = subprocess.run(['pdflatex', '-no-file-line-error' 
+    cmd = subprocess.run(['/bin/pdflatex', '-no-file-line-error' 
                                       'latex/output.tex'], shell=False)
     cmd
-    cmd = subprocess.run(['evince output.pdf', 'output.pdf'], shell=False)
+    cmd = subprocess.run(['/bin/evince output.pdf', 'output.pdf'], shell=False)
     cmd
